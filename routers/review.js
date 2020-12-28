d99b90f812dd7c79a07ed9172ad12163b3a09d26
Wei Song <ws446@cornell.edu>
2020-12-28 20:19:27 +0800

    add review flashcard filter

--- routers/review.js
+++ routers/review.js
@@ -66,4 +66,77 @@ router.delete(
   }
 );
 
+router.post(
+  "/review",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      let flashcards = await Flashcard.find({ author: req.user._id });
+      let query = req.body;
+      let resCards = filter(flashcards, query);
+      res.status(200).send(resCards);
+    } catch (error) {
+      console.log(error.toString());
+      res.status(500).send(error);
+    }
+  }
+);
+
+const filter = (flashcards, query) => {
+  let categoriesSet, tagsSet;
+  if (query.categories) {
+    categoriesSet = new Set();
+    for (let category of query.categories) categoriesSet.add(category);
+  }
+  if (query.tags) {
+    tagsSet = new Set();
+    for (let tag of query.tags) tagsSet.add(tag);
+  }
+
+  let eligibleFlashcards = flashcards.filter((flashcard) => {
+    if (categoriesSet && !categoriesSet.has(flashcard.category)) return false;
+    if (tagsSet) {
+      let flag = false;
+      for (let tag of flashcard.tags) {
+        if (tagsSet.has(tag)) {
+          flag = true;
+          break;
+        }
+      }
+      if (!flag) return false;
+    }
+    if (
+      flashcard.reviewRecord &&
+      flashcard.reviewRecord[0] &&
+      moment(flashcard.reviewRecord[0]).format("YYYYMMDD") ===
+        moment().format("YYYYMMDD")
+    ) {
+      return false;
+    }
+    return true;
+  });
+
+  let score = eligibleFlashcards.map((flashcard) => {
+    let thisScore = Math.random();
+    if (query.priority === true) {
+      thisScore = 0;
+      if (flashcard.reviewRecord && flashcard.reviewRecord[0]) {
+        thisScore = moment(flashcard.reviewRecord[0]).valueOf();
+      }
+    }
+    return [thisScore, flashcard];
+  });
+
+  score.sort((a, b) => {
+    return a[0] - b[0];
+  });
+
+  let res = [];
+  let limit = query.amount ? query.amount : Number.MAX_VALUE;
+  for (let i = 0; i < Math.min(limit, score.length); i++) {
+    res.push(score[i][1]._id);
+  }
+  return res;
+};
+
 module.exports = router;
